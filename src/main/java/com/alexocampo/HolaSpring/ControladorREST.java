532bc2c01c2e66df2e4be4adf5e3cec9acb6e1b8
alexoca1 <alexanderocaher@gmail.com>
2023-04-27 11:24:19 +0200

    commit 2

--- src/main/java/com/alexocampo/HolaSpring/ControladorREST.java
+++ src/main/java/com/alexocampo/HolaSpring/ControladorREST.java
@@ -1,6 +1,10 @@
 package com.alexocampo.HolaSpring;
 
 import ch.qos.logback.classic.spi.ThrowableProxyUtil;
+import com.alexocampo.domain.Individuo;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.ui.Model;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,9 +22,33 @@ public class ControladorREST {
         public String comienzo(Model model) {
                 String hola = "Hola mundo con Spring Boot";
 
+                Individuo individuo = new Individuo();
+                individuo.setNombre("Andres");
+                individuo.setApellido("Marin");
+                individuo.setEdad("26");
+                individuo.setCorreo("andres@gmail.com");
+                individuo.setTelefono("4387264538");
+
+                Individuo individuo2 = new Individuo();
+                individuo2.setNombre("Karla");
+                individuo2.setApellido("Martinez");
+                individuo2.setEdad("35");
+                individuo2.setCorreo("karla@gmail.com");
+                individuo2.setTelefono("456546432");
+                
+             //   List<Individuo> individuos= new ArrayList();
+              //  individuos.add(individuo);
+              //  individuos.add(individuo2);
+                
+                List individuos = Arrays.asList(individuo, individuo2);
+
+                log.info("estoy ejecutando el controlador MVC");
+
                 model.addAttribute("hola", hola);
                 model.addAttribute("hola2", hola2);
-                log.info("estoy ejecutando el controlador MVC");
+                model.addAttribute("individuo", individuo);
+                model.addAttribute("individuos", individuos);
+
                 //log.debug("Mas informacion");
                 return "indice";
         }
---
+++ src/main/java/com/alexocampo/domain/Individuo.java
@@ -0,0 +1,16 @@
+
+package com.alexocampo.domain;
+
+import lombok.Data;
+
+@Data
+public class Individuo {
+        
+        private String nombre;
+        private String apellido;
+        private String correo;
+        private String Telefono;
+        private String edad;
+        
+        
+}
